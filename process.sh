77603190532d793d3abe3299872740818e777a78
Collin D <cjdekalb@gmail.com>
2021-08-23 15:21:59 -0700

    remove prettier

--- process.sh
+++ process.sh
@@ -6,6 +6,5 @@ sed -i -r "s/(<div class.+)/$div_end\1/" out.html
 sed -i -r "s/^$div_end(.+contacts)/\1/" out.html
 sed -i "s/<\/body>/$div_end\n&/" out.html
 sed -i "s/<head>/&\n<link rel=\"stylesheet\" href=\"styles.css\">/" out.html
-prettier --write out.html
 wkhtmltopdf --enable-local-file-access out.html processed_resume.pdf
 rm out.html
