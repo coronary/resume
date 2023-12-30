d8423fa65acbe44938adef35ea80e80e52a051e8
Vera De Kalb <vera.dekalb@brandingbrand.com>
2023-12-29 16:53:15 -0800

    start with out file and don't remove

--- process.sh
+++ process.sh
@@ -6,4 +6,3 @@ sed -i '' "s/^$div_end\(.+contacts\)/\1/" "out.html"
 sed -i '' "s/<\/body>/$div_end\n&/" "out.html"
 sed -i '' "s/<head>/&\n<link rel=\"stylesheet\" href=\"styles.css\">/" "out.html"
 wkhtmltopdf --enable-local-file-access out.html processed_resume.pdf
-rm out.html
