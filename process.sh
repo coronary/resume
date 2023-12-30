5268382656e18387a74728ce80a388fed463fac2
Vera De Kalb <vera.dekalb@brandingbrand.com>
2023-12-29 16:38:29 -0800

    script version for mac BSD systems

--- process.sh
+++ process.sh
@@ -1,9 +1,9 @@
 div_end="<\/div>"
-pandoc -s res.md -o out.html
-sed -i -r "s/(<h2 id=(\"\w+\").+)/<div class=\2>\n\1/" out.html
-sed -i -r "s/(<div class.+)/$div_end\1/" out.html
-sed -i -r "s/^$div_end(.+contacts)/\1/" out.html
-sed -i "s/<\/body>/$div_end\n&/" out.html
-sed -i "s/<head>/&\n<link rel=\"stylesheet\" href=\"styles.css\">/" out.html
+pandoc -so res.md out.html
+sed -i '' "s/\(<h2 id=\(\"\w+\"\).+\)/<div class=\2>\n\1/" "out.html"
+sed -i '' "s/\(<div class.+\)/$div_end\1/" "out.html"
+sed -i '' "s/^$div_end\(.+contacts\)/\1/" "out.html"
+sed -i '' "s/<\/body>/$div_end\n&/" "out.html"
+sed -i '' "s/<head>/&\n<link rel=\"stylesheet\" href=\"styles.css\">/" "out.html"
 wkhtmltopdf --enable-local-file-access out.html processed_resume.pdf
 rm out.html
