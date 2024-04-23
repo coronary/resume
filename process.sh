938110436b3d1c275575d56dfe98bcc267ff7eb3
coronary <veradekalb@gmail.com>
2024-04-22 19:51:31 -0500

    fix: added pdf suffix

--- process.sh
+++ process.sh
@@ -1,6 +1,6 @@
 div_end="<\/div>"
 current_date=$(date -u +%m-%d-%Y)
-resume_filename="${current_date}_resume"
+resume_filename="${current_date}_resume.pdf"
 pandoc -s res.md -o out.html
 sed -i '' "s/\(<h2 id=\(\"\w+\"\).+\)/<div class=\2>\n\1/" "out.html"
 sed -i '' "s/\(<div class.+\)/$div_end\1/" "out.html"
